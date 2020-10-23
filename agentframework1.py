e151ced81e5326a8318f14e98cceaf48d00ddddd
gladyskenyon <71322257+gladyskenyon@users.noreply.github.com>
2020-10-23 17:28:11 +0100

    Add files via upload

--- agentframework1.py
+++ agentframework1.py
@@ -7,38 +7,68 @@ Created on Thu Oct 22 11:57:30 2020
 
 import random 
 
+
+# Create the Agent class
 class Agent:
     def __init__ (self, environment, agents, neighbourhood):
-        
-        self.x = random.randint(0,300)
-        self.y = random.randint(0,300)
         
         self.environment = environment
         self.store = 0 
         self.agents = agents
         
+        self._x = random.randint(0,300)
+        self._y = random.randint(0,300)
+        
+# Protect x and y using get and set properties
+
+    def getx(self):
+        return self._x
+    def setx(self, value):
+        self._x = value
+
+    def gety(self):
+        return self._y
+    def sety(self, value):
+        self._y = value
+
+      
     def move(self):
         
         if random.random() < 0.5:
-            self.x = (self.x + 1) % 300
+            self._x = (self._x + 1) % 300
         else:
-            self.x = (self.x - 1) % 300
+            self._x = (self._x - 1) % 300
         
         if random.random() < 0.5:
-            self.y = (self.y + 1) % 300
+            self._y = (self._y + 1) % 300
         else:
-            self.y = (self.y - 1) % 300
+            self._y = (self._y - 1) % 300
 
 
     def eat(self):
         
-        if self.environment[self.y][self.x] > 10:
+        if self.environment[self._y][self._x] > 10:
             
-            self.environment[self.y][self.x] -= 10
-            self.store += 10
+            self.environment[self._y][self._x] -= 10
             
+            self.store += 20
+            
+        elif self.environment[self._y][self._x] >0:
+            
+            self.environment[self._y][self._x] -= 1
+            self.store += 1
+    
+    
+    def sick(self):
+        
+        if self.store >= 100:
+            if self.store + self.environment[self._y][self._x] <250:
+                self.environment[self._y][self._x] += (self.store)
+                self.store -= (250 - self.store)
         
     def share_with_neighbours(self, neighbourhood): 
+        
+        self.neighhbourhood = neighbourhood
         
         for agent in self.agents:
             
@@ -50,8 +80,7 @@ class Agent:
                 self.store = ave
                 agent.store = ave
                 
-#                print("sharing " + str(dist) + " " + str(ave))
 
     def distance_between(self, agent):
-        return (((self.x - agent.x)**2) + ((self.y - agent.y)**2))**0.5    
+        return (((self._x- agent._x)**2) + ((self._y- agent._y)**2))**0.5    
         
--- finalmodel.py
+++ finalmodel.py
@@ -5,138 +5,170 @@ Created on Thu Oct 22 11:55:21 2020
 @author: sggkenyo
 """
 
+# Upload required libraries
 import random 
-import operator
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-import agentframework1
+import matplotlib 
+matplotlib.use('TkAgg')
+import tkinter
 import csv
+# Import agent class
+import agentframework1
 
 
-# Create environment, upload file, read csv
-environment = []
+
+# Create environment 
+# Use CSV reader code to read in text file
 f = open('in.txt', newline = '')
 reader = csv.reader(f, quoting = csv.QUOTE_NONNUMERIC)
 
+# Create empty environment list
+environment = []
 
-# join environment to rowlist
+# Shift data into 2-D list
 for line in reader:	
-    rowlist = [] 
+    rowlist = []
+# Append each value to rowlist, then rowlist to environment list   
     for value in line:
         rowlist.append(value)
     environment.append(rowlist)
     
-f.close()    
+    
 
-#create variables
-agents = []
-
+# Set Variables    
+# For the number of agents
 num_of_agents = 50
 
-num_of_iterations = 100
+# For the number of model iterations
+num_of_iterations = 50
 
-neighbourhood = 20
+# For the proximity of agents to share resources
+neighbourhood = 40
 
 
-# set figure and axis parameters
+
+# Set up figure and axis
 fig = matplotlib.pyplot.figure(figsize=(7, 7))
 ax = fig.add_axes([0, 0, 1, 1])
 
-#ax.set_autoscale_on(False)
-
-#sharelist of agents with each agent
-for i in range(num_of_agents):
-   agents.append(agentframework1.Agent(environment, agents, neighbourhood))
-
-
-# check the agentframework file is connected
-a = agentframework1.Agent(environment, agents, neighbourhood)
-print(a.y, a.x)
-
-a.move()
-print(a.y, a.x)
-
-
- #plot axis and environment 
+# Define x and y axis scale
 matplotlib.pyplot.ylim(0, 300)
 matplotlib.pyplot.xlim(0,300)
 
+# Set autoscaling on plots
+ax.set_autoscale_on(False)
 
-#activate agent methods
+
+
+# Create agents
+# Create empty list for agents
+agents = []
+
+# Give each agent access to info on others
+for i in range(num_of_agents):
+    agents.append(agentframework1.Agent(environment, agents, neighbourhood))
+
+# Check the agentframework file is connected using print
+a = agentframework1.Agent(environment, agents, neighbourhood)
+print(a._y, a._x)
+
+# Check the co-ordinates move
+a.move()
+print(a._y, a._x)
+
+
+# Activate agents
 for j in range(num_of_iterations):
-    
+    # Use this to randomize
     random.shuffle(agents)    
             
     for i in range(num_of_agents):
                 
         agents[i].move()
         agents[i].eat()
-        agents[i].share_with_neighbours(neighbourhood)
+        agents[i].share_with_neighbours
+        agents[i].sick()
+        agents[i].distance_between
 
-#Animate model
 
-#carry_on = True
-
+# Animate model
+# Create update function
 def update(frame_number):
     
     fig.clear()
-    
-    global carry_on
-    
-    
+          
+       
     for i in range(num_of_agents):
         
             if random.random() < 0.5:
                 
-                agents[i].x  = (agents[i].x + 1) % 300
+                agents[i]._x  = (agents[i]._x + 1) % 300
                 
             else:
                 
-                agents[i].x  = (agents[i].x - 1) % 300
+                agents[i]._x  = (agents[i]._x - 1) % 300
             
             if random.random() < 0.5:
                 
-                agents[i].y  = (agents[i].y + 1) % 300
+                agents[i]._y  = (agents[i]._y + 1) % 300
                 
             else:
                 
-                agents[i].y  = (agents[i].y - 1) % 300
-                
-            if random.random() < 0.1: 
-                
-                carry_on = False
-                print("stopping condition")
+                agents[i]._y  = (agents[i]._y - 1) % 300
+
+
+     
        
-    matplotlib.pyplot.imshow(environment)
-    
-    for i in range(num_of_agents):
-        
-        matplotlib.pyplot.scatter(agents[i].x,agents[i].y, marker='x', color='deeppink') 
-        matplotlib.pyplot.show()
-        
-        
-# create stopping function
-def gen_function(b = [0]):
-    
-    a = 0
-    global carry_on
-    
-    while (a < 10) & (carry_on) :
-        
-        yield a 
-        a = a + 1
+    plt.imshow(environment)
+
+    for i in range(num_of_agents):    
+        matplotlib.pyplot.scatter(agents[i]._x,agents[i]._y, marker='x', color='blue') 
 
 
-animation = matplotlib.animation.FuncAnimation(fig, update, interval=1, repeat=False, frames=5)
+# Start animation for 10 frames
+def run():
+    animation = matplotlib.animation.FuncAnimation(fig, update, interval=1, repeat=False, frames=num_of_iterations)  
+    # Use becuase of GUI 
+    canvas.draw()
 
-matplotlib.pyplot.show()
-fig.show()
+# Set up GUI
+# Build the main window
+root = tkinter.Tk()
+root.wm_title("Model")
+canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=root)
+canvas._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+
+# Build menu bar with option to run model
+menu_bar = tkinter.Menu(root)
+root.config(menu=menu_bar)
+model_menu = tkinter.Menu(menu_bar)
+menu_bar.add_cascade(label="Model", menu=model_menu)
+model_menu.add_command(label="Run model", command=run)
 
 
 
-#save environment as csv
+
+
+# Write out Files
+totalstore = 0 
+for agent in agents:
+    totalstore = agent.store + totalstore
+print("Total consumption:", totalstore)
+
+# Write out environment as file
 f2= open('environmentout.csv', 'w', newline='')
-writer= csv.writer(f2, delimiter=' ')
+writer= csv.writer(f2, delimiter=',')
+'''
+# Write out total amount stored by each agent
+f3= open('agentout.csv', 'w', newline='')
+writer= csv.writer(f3, delimiter=',')
+
+'''
 
 for row in environment:
     writer.writerow(row)
+f2.close 
+
+tkinter.mainloop()
